ed991f5f1218343ecb769079b41cc091c627057d
Specify your username <Your email address>
2024-05-31 08:42:54 +0200

    final commit

--- script/index.js
+++ script/index.js
@@ -1,33 +1,34 @@
-calculate = document.getElementById('calculate')
-calculate.addEventListener('click', (event) => {
-    event.preventDefault()
-    calculate()
+// JORDAN SPRINGVELDT
+
+calculate_button = document.getElementById('calculate_button');
+calculate_button.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('clicked');
+    calculate();
 })
+console.log('click addEventListener');
 
 function calculate() {
-    const prices = document.querySelectorAll('.price');
-    const quantities = document.querySelectorAll('.quantity input');
-    const totals = document.querySelectorAll('[total]');
-    let femaleTotal = 0;
-    let maleTotal = 0;
-    for (let i = 0; i < prices.length; i++) {
-        const price = parseFloat(prices[i].textContent);
-        const quantity = parseInt(quantities[i].value);
-        const total = price * quantity;
-        totals[i].textContent = total.toFixed(2);
-        if (i === 0) {
-            femaleTotal += total;
-        } else if (i === 1) {
-            maleTotal += total;
-        }
-    }
-    // Calculate the overall total by adding female and male totals
-    const overallTotal = femaleTotal + maleTotal;
-    // Display the individual totals and overall total
-    const femaleTotalElement = document.querySelector('[female-total]');
-    femaleTotalElement.textContent = ` R${femaleTotal.toFixed(2)}`;
-    const maleTotalElement = document.querySelector('[male-total]');
-    maleTotalElement.textContent = ` R${maleTotal.toFixed(2)}`;
-    const overallTotalElement = document.querySelector('[overall-total]');
-    overallTotalElement.textContent = `R${overallTotal.toFixed(2)}`;
+    m_price = document.getElementById('m_price').innerText
+    console.log('m_price: ' + m_price);
+    f_price = document.getElementById('f_price').innerText
+    console.log('f_price: ' + f_price);
+
+    m_quantity = document.getElementById('m_quantity').value;
+    console.log('m_quantity: ' + m_quantity);
+    f_quantity = document.getElementById('f_quantity').value;
+    console.log('f_quantity: ' + f_quantity);
+
+    m_total_value = (m_price * m_quantity).toFixed(2);
+    console.log('m_total_value: ' + m_total_value);
+    f_total_value = (f_price * f_quantity).toFixed(2);
+    console.log('f_total_value: ' + f_total_value);
+
+    sum_total = parseFloat(m_total_value) + parseFloat(f_total_value);
+    console.log('sum_total: ' + sum_total);
+
+    document.getElementById('m_total').innerText = 'R ' + m_total_value;
+    document.getElementById('f_total').innerText = 'R ' + f_total_value;
+    document.getElementById('sum_total').innerText = 'R ' + sum_total;
+
 }
